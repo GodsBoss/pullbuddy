a92b4c95a62016d2f7b71cd37d038d927c11998c
Timo Reitz <godsboss@gmx.de>
2019-06-15 12:55:02 +0200

    Outline command structure.

--- cmd/pullbuddy/main.go
+++ cmd/pullbuddy/main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/urfave/cli.v2"
+)
+
+func main() {
+	if err := createApp().Run(os.Args); err != nil {
+		fmt.Printf("Error: %+v\n", err)
+		os.Exit(1)
+	}
+}
+
+func createApp() *cli.App {
+	app := &cli.App{
+		Name:    "pullbuddy",
+		Version: "1.0.0",
+		Commands: []*cli.Command{
+			{
+				Name: "server",
+				Subcommands: []*cli.Command{
+					{
+						Name: "start",
+						Action: func(ctx *cli.Context) error {
+							return nil
+						},
+					},
+				},
+			},
+			{
+				Name: "schedule",
+				Action: func(ctx *cli.Context) error {
+					return nil
+				},
+			},
+			{
+				Name: "status",
+				Action: func(ctx *cli.Context) error {
+					return nil
+				},
+			},
+		},
+	}
+	return app
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/GodsBoss/pullbuddy
 
 go 1.12
+
+require (
+	github.com/urfave/cli v1.20.0
+	gopkg.in/urfave/cli.v2 v2.0.0-20180128182452-d3ae77c26ac8
+)
